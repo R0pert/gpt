2e8c77f90a6229787f72bc142ecdc1f8b4142c63
R0pert <87825741+R0pert@users.noreply.github.com>
2023-11-17 11:26:28 +0330

    constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 # Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
 # and rename this file to constants.py.
-apikey = "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"
+OPENAI_API_KEY = "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"
